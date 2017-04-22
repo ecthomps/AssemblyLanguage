a163ea6b0997dc0dca27fc2013325e45fbb07408
Eranus Thompson <ecthomps@umich.edu>
2017-04-22 15:17:54 -0400

    created class hexNumLibr, hexTable

--- passAssembler/passAssembler/Source.cpp
+++ passAssembler/passAssembler/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <fstream>
@@ -76,6 +77,354 @@ void fillHexNum(string* numVal, int numMem) {
 	for (int i = numVal->size(); i < numMem; i++)
 		*numVal = "0" + *numVal;
 }
+
+class hexNumLibr {
+
+	public:
+		void setHex(string val) {
+			hexVal = val;
+			sDigit = val.size();
+		}
+
+		char revertHex(int decVal) {
+			if (decVal == 0) 
+				return '0';			
+			else if (decVal == 1) 
+				return '1';			
+			else if (decVal == 2) 
+				return '2';			
+			else if (decVal == 3) 
+				return '3';			
+			else if (decVal == 4) 
+				return '4';			
+			else if (decVal == 5) 
+				return '5';			
+			else if (decVal == 6) 
+				return '6';			
+			else if (decVal == 7) 
+				return '7';			
+			else if (decVal == 8) 
+				return '8';			
+			else if (decVal == 9) 
+				return '9';			
+			else if (decVal == 10) 
+				return 'A';			
+			else if (decVal == 11) 
+				return 'B';			
+			else if (decVal == 12) 
+				return 'C';			
+			else if (decVal == 13) 
+				return 'D';			
+			else if (decVal == 14) 
+				return 'E';			
+			else if (decVal == 15)
+				return 'F';
+			else
+				return 'x';
+		}
+
+		int convertDecimal(char decVal) {
+			if (decVal == '0')
+				return 0;
+			else if (decVal == '1')
+				return 1;
+			else if (decVal == '2')
+				return 2;
+			else if (decVal == '3')
+				return 3;
+			else if (decVal == '4')
+				return 4;
+			else if (decVal == '5')
+				return 5;
+			else if (decVal == '6')
+				return 6;
+			else if (decVal == '7')
+				return 7;
+			else if (decVal == '8')
+				return 8;
+			else if (decVal == '9')
+				return 9;
+			else if (decVal == 'A')
+				return 10;
+			else if (decVal == 'B')
+				return 11;
+			else if (decVal == 'C')
+				return 12;
+			else if (decVal == 'D')
+				return 13;
+			else if (decVal == 'E')
+				return 14;
+			else if (decVal == 'F')
+				return 15;
+			else
+				return 0;
+		}
+
+		string addHex(hexNumLibr hexNum) {
+			int val = sDigit - 1;
+			int carry = 0, sum;
+			char val1, val2;
+			for (int i = hexNum.sDigit - 1; i != -1; i--) {
+				val1 = hexVal[val];
+				val2 = hexNum.hexVal[i];		
+
+				int sum;
+				sum = convertDecimal(val1) + convertDecimal(val2) + carry;
+				if (sum >= 16) {
+					carry = 1;
+					sum = sum - 16;
+				}
+				else
+					carry = 0;
+
+				hexVal[val] = revertHex(sum);
+				val--;
+			}
+			sum = 0;
+			while (carry > 0)	{
+				if (val == -1)	{
+					hexVal.insert(hexVal.begin(), revertHex(1));
+					sDigit = sDigit + 1;
+					val++;
+					carry = 0;
+				}
+				val1 = hexVal[val];
+				sum = convertDecimal(val1) + carry;
+				if (sum >= 16) {
+					carry = 1;
+					sum -= 16;
+					hexVal[val] = revertHex(sum);
+					if (val == 0 && carry == 1) 	{
+						hexVal.insert(hexVal.begin(), revertHex(1));
+						sDigit = sDigit + 1;
+						val++;
+						carry = 0;
+					}
+					val--;
+				}
+				else {
+					carry = 0;
+					hexVal[val] = revertHex(sum);
+					val--;
+				}
+			}
+			return hexVal;
+		}
+
+		string getHex() {
+			return hexVal;
+		}	
+
+private:
+	string hexVal;
+	int sDigit;	
+};
+
+string clearSpaces(string fixStr) {
+	stringstream getString;
+	string newString;
+
+	getString << fixStr;
+	getString >> newString;
+	getString.flush();
+
+	return newString;
+}
+
+class hexTable
+{
+	struct SymbolEn
+	{
+		string token;
+		string memLoc;
+
+	};
+
+private:
+	vector<SymbolEn> hexTable;
+
+public:
+	string findEntry(string numVal) {
+		for (string::size_type i = 0; i<hexTable.size(); i++) {
+			if (hexTable[i].token == numVal)
+				return hexTable[i].memLoc;
+		}
+	}
+
+	void addEntry(string x, string y) {
+
+	}
+
+	int getSize() {
+		return hexTable.size();
+	}
+
+	string getSymbol(int val) {
+		return hexTable[val].token;
+	}
+
+	string getAddress(int val) {
+		return hexTable[val].memLoc;
+	}
+};
+
+class TwoAssembler {
+	
+	public:
+		void inputDataItems() {
+
+			fstream fin;
+			fin.open("dataFile.txt");
+			cout << "Collecting Data from Data.txt" << endl;
+
+			if (fin)
+			{
+				string TestString;
+
+				while (fin)
+				{
+					getline(fin, TestString);
+					macLine.push_back(TestString);
+
+
+				}
+			}
+			else
+				cout << "Could not read the file";
+
+			string lastAddress;
+			entryTemp.memLoc = "";
+			entryTemp.macOper = "";
+			entryTemp.oper = "";
+			entryTemp.comMand = "";
+
+			for (string::size_type i = 0; i<macLine.size() - 1; i++)
+			{
+
+				entryTemp.memLoc = "";
+				entryTemp.macOper = "";
+				entryTemp.oper = "";
+				entryTemp.comMand = "";
+
+				if (isLabel(i))
+				{
+					string wString = macLine[i].substr(10, 10);
+					wString = clearSpaces(wString);
+					if (wString == "ORG") {
+						entryTemp.memLoc = wString.substr(5, 3);
+						lastAddress = entryTemp.memLoc;
+					}
+					else {
+						string Label;
+						Label = macLine[i].substr(0, 10);
+						Label = clearSpaces(Label);
+						symTable.addEntry(Label, lastAddress);
+						string machineCode = getCommand(i);
+
+						if (machineCode == "END")
+							break;
+
+						entryTemp.macOper = machineCode;
+						entryTemp.memLoc = lastAddress;
+
+						int resolveAddress = getLocation(machineCode);
+
+						hexNumLibr addressHex, incrementHex;
+						char temp = incrementHex.revertHex(resolveAddress);
+						string toSetIncrement;
+
+						toSetIncrement.push_back(temp);
+						addressHex.setHex(lastAddress);
+						incrementHex.setHex(toSetIncrement);
+						addressHex.addHex(incrementHex);
+						lastAddress = addressHex.getHex();
+						outputEnt.push_back(entryTemp);
+					}
+				}
+				else {
+					string wString = macLine[i].substr(10, 10);
+					wString = clearSpaces(wString);
+					if (wString == "ORG")
+					{
+						entryTemp.memLoc = macLine[i].substr(21, 3);
+						lastAddress = entryTemp.memLoc;
+					}
+					else {
+						string machineCode = getCommand(i);
+
+						if (machineCode == "END")
+							break;
+
+						entryTemp.macOper = machineCode;
+						entryTemp.memLoc = lastAddress;
+
+						int resolveAddress = getLocation(machineCode);
+						hexNumLibr addressHex, incrementHex;
+						char temp = incrementHex.revertHex(resolveAddress);
+						string toSetIncrement;
+
+						toSetIncrement.push_back(temp);
+						addressHex.setHex(lastAddress);
+						incrementHex.setHex(toSetIncrement);
+						addressHex.addHex(incrementHex);
+						lastAddress = addressHex.getHex();
+						outputEnt.push_back(entryTemp);
+					}
+
+				}
+			}
+		}
+
+		void operTable() {
+			
+		}
+
+		void printLines() {
+			ofstream fout("dataOUt.txt");
+
+			cout << "Inputting Data to Output.txt" << endl;
+			fout << "******************************************************************************" << endl;
+			fout << "Input Data:" << endl;
+
+			for (string::size_type i = 0; i< macLine.size() - 1; i++)
+				fout << macLine[i] << endl;
+
+			fout << "******************************************************************************" << endl << endl;
+			fout << "Output Data:" << endl;
+
+			fout << "Address" << "\t\t\t" << "Machine Code" << "\t\t\t" << "Operands"
+				<< "\t\t\t" << "Instructions" << endl;
+
+			for (string::size_type i = 0; i <outputEnt.size(); i++) {
+				fout << outputEnt[i].memLoc << "\\t\t\t" << outputEnt[i].macOper
+					<< "\t\t\t" << outputEnt[i].oper << "\t\t\t" << outputEnt[i].comMand
+					<< endl;
+			}
+
+			fout << endl << endl << endl;
+			fout << "Symbol" << "\t\t" << "Address" << endl;
+
+			for (int i = 0; i< symTable.getSize() - 1; i++)
+				fout << symTable.getSymbol(i) << "      " << symTable.getAddress(i) << endl;
+
+			fout << "******************************************************************************" << endl;
+		}
+
+	private:
+		struct OutlineEntry {
+			string memLoc, macOper;
+			string oper, comMand;
+		};
+
+		vector<string> macLine;
+		vector<OutlineEntry> outputEnt;
+		OutlineEntry entryTemp;
+
+		bool isLabel(int val);
+		string getCommand(int val);
+		int getLocation(string machineCode);
+		hexTable symTable;
+};
 
 class passAssem {
 public:
